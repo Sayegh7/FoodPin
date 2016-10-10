e7ce0bf0985e0f5617c802a1897f0769268f2c42
Ahmed El Sayegh <ahmedelsayegh7@gmail.com>
2016-10-10 04:07:20 +0300

    Share and delete buttons

---
+++ RestaurantTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Ahmed El Sayegh on 10/9/16.
+//  Copyright © 2016 Ahmed El Sayegh. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
